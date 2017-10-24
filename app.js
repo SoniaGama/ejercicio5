7bd69997d89c482b7c3ce946e8ea0a5a8c13861c
SoniGama <abinyuuu@gmail.com>
2017-10-24 15:24:55 -0500

    cambios

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var comparacion = function(array1, array2){
-  document.write("funcion declara, es una prueba");
+  if (array1.length!==array2.length){
+    return false;
+  }else{
+    for (var i = 0; i < array.length; i++) {
+        console.log(array1,array2);
+      if(array1[i]!==array[i]){
+        document.write("Los arreglos son iguales");
+        return false;
 
+      }
+    }
+    return true;
+  }
 }
 
 var arreglo1 = [1,2,3,4,5,6,7,8,9,0];
